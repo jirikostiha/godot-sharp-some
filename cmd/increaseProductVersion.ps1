1cf93dab079e7d8d459e5420f3d226f3eac141f5
Jiri Kostiha <ijkdata@gmail.com>
2022-11-05 15:12:08 +0100

    update script for increasing version

---
+++ cmd/increaseProductVersion.ps1
@@ -0,0 +1,30 @@
+param(
+    [switch] $Minor
+)
+
+$scriptDescription = "update product version, commit, push"
+$scriptVersion = "1.2"
+$svn = "git"
+
+$versionFile = "..\product_version.props"
+
+write-host "updating product version.. " -NoNewline; write-verbose "(script version: $scriptVersion)"
+[xml]$versionFileXml = Get-Content $versionFile
+$oldVersion = New-Object System.Version($versionFileXml.Project.PropertyGroup.VersionPrefix)
+if ($Minor) {
+    $newVersion = New-Object System.Version($oldVersion.Major, ($oldVersion.Minor + 1), 0)
+}
+else {
+    $newVersion = New-Object System.Version($oldVersion.Major, $oldVersion.Minor, ($oldVersion.Build + 1))
+}
+
+"$oldVersion -> $newVersion"
+$versionFileXml.Project.PropertyGroup.VersionPrefix = $newVersion.ToString()
+
+$versionFileXml.Save($versionFile)
+
+"publishing product version.."
+git add $versionFile
+$message = "product: increase version $oldVersion -> $newVersion"
+git commit -m $message
+git push
