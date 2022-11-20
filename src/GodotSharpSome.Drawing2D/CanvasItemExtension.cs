4008d76b29ddf59d4d4ca7256bfd0309166fdd77
Jiri Kostiha <ijkdata@gmail.com>
2022-11-20 08:28:22 +0100

    move optional params to new line

--- src/GodotSharpSome.Drawing2D/CanvasItemExtension.cs
+++ src/GodotSharpSome.Drawing2D/CanvasItemExtension.cs
@@ -7,17 +7,20 @@
 
     public static class CanvasItemExtension
     {
-        public static void DrawDot(this CanvasItem canvas, Vector2 position, Color color, bool antialiased = false)
+        public static void DrawDot(this CanvasItem canvas, Vector2 position, Color color,
+            bool antialiased = false)
         {
             canvas.DrawMultiline(Multiline.Dot(position), color, 1, antialiased);
         }
 
-        public static void DrawDots(this CanvasItem canvas, IEnumerable<Vector2> positions, Color color, bool antialiased = false)
+        public static void DrawDots(this CanvasItem canvas, IEnumerable<Vector2> positions, Color color,
+            bool antialiased = false)
         {
             canvas.DrawMultiline(Multiline.Dots(positions), color, 1, antialiased);
         }
 
-        public static void DrawCircleLine(this CanvasItem canvas, Vector2 center, float radius, Color lineColor, float lineWidth = 1, bool antialiased = false)
+        public static void DrawCircleLine(this CanvasItem canvas, Vector2 center, float radius, Color lineColor,
+            float lineWidth = 1, bool antialiased = false)
         {
             canvas.DrawArc(center, radius, 0, Pi * 2, (int)radius * 16, lineColor, lineWidth, antialiased);
         }
@@ -27,13 +30,15 @@
             canvas.DrawCircle(center, radius, areaColor);
         }
 
-        public static void DrawCircle(this CanvasItem canvas, Vector2 center, float radius, Color lineColor, Color areaColor, float lineWidth = 1, bool antialiased = false)
+        public static void DrawCircle(this CanvasItem canvas, Vector2 center, float radius, Color lineColor, Color areaColor,
+            float lineWidth = 1, bool antialiased = false)
         {
             canvas.DrawCircleArea(center, radius, areaColor);
             canvas.DrawCircleLine(center, radius, lineColor, lineWidth, antialiased);
         }
 
-        public static void DrawTriangleLine(this CanvasItem canvas, Vector2 a, Vector2 b, Vector2 c, Color lineColor, float lineWidth = 1, bool antialiased = false)
+        public static void DrawTriangleLine(this CanvasItem canvas, Vector2 a, Vector2 b, Vector2 c, Color lineColor,
+            float lineWidth = 1, bool antialiased = false)
         {
             canvas.DrawMultiline(
                 Multiline.Triangle(a, b, c),
@@ -47,20 +52,23 @@
                 new Color[] { areaColor });
         }
 
-        public static void DrawTriangle(this CanvasItem canvas, Vector2 a, Vector2 b, Vector2 c, Color lineColor, Color areaColor, float lineWidth = 1, bool antialiased = false)
+        public static void DrawTriangle(this CanvasItem canvas, Vector2 a, Vector2 b, Vector2 c, Color lineColor, Color areaColor,
+            float lineWidth = 1, bool antialiased = false)
         {
             canvas.DrawTriangleArea(a, b, c, areaColor);
             canvas.DrawTriangleLine(a, b, c, lineColor, lineWidth, antialiased);
         }
 
-        public static void DrawRectangleLine(this CanvasItem canvas, Vector2 center, float length, float width, float rotationAngle, Color lineColor, float lineWidth = 1, bool antialiased = false)
+        public static void DrawRectangleLine(this CanvasItem canvas, Vector2 center, float length, float width, float rotationAngle, Color lineColor,
+            float lineWidth = 1, bool antialiased = false)
         {
             canvas.DrawMultiline(
                 Multiline.Rectangle(center, length / 2, width / 2, rotationAngle),
                 lineColor, lineWidth, antialiased);
         }
 
-        public static void DrawRectangleLine(this CanvasItem canvas, Vector2 vertex1, Vector2 vertex2, float height, Color lineColor, float lineWidth = 1, bool antialiased = false)
+        public static void DrawRectangleLine(this CanvasItem canvas, Vector2 vertex1, Vector2 vertex2, float height, Color lineColor,
+            float lineWidth = 1, bool antialiased = false)
         {
             canvas.DrawMultiline(
                 Multiline.Rectangle(vertex1, vertex2, height),
@@ -81,19 +89,22 @@
                 new Color[] { areaColor });
         }
 
-        public static void DrawRectangle(this CanvasItem canvas, Vector2 center, float length, float width, float rotationAngle, Color lineColor, Color areaColor, float lineWidth = 1, bool antialiased = false)
+        public static void DrawRectangle(this CanvasItem canvas, Vector2 center, float length, float width, float rotationAngle, Color lineColor, Color areaColor,
+            float lineWidth = 1, bool antialiased = false)
         {
             canvas.DrawRectangleArea(center, length, width, rotationAngle, areaColor);
             canvas.DrawRectangleLine(center, length, width, rotationAngle, lineColor, lineWidth, antialiased);
         }
 
-        public static void DrawRectangle(this CanvasItem canvas, Vector2 vertex1, Vector2 vertex2, float height, Color lineColor, Color areaColor, float lineWidth = 1, bool antialiased = false)
+        public static void DrawRectangle(this CanvasItem canvas, Vector2 vertex1, Vector2 vertex2, float height, Color lineColor, Color areaColor,
+            float lineWidth = 1, bool antialiased = false)
         {
             canvas.DrawRectangleArea(vertex1, vertex2, height, areaColor);
             canvas.DrawRectangleLine(vertex1, vertex2, height, lineColor, lineWidth, antialiased);
         }
 
-        public static void DrawRegularConvexPolygonLine(this CanvasItem canvas, Vector2 center, float radius, int verticesCount, float rotationAngle, Color lineColor, float lineWidth = 1, bool antialiased = false)
+        public static void DrawRegularConvexPolygonLine(this CanvasItem canvas, Vector2 center, float radius, int verticesCount, float rotationAngle, Color lineColor,
+            float lineWidth = 1, bool antialiased = false)
         {
             canvas.DrawMultiline(
                 Multiline.RegularConvexPolygon(center, radius, verticesCount, rotationAngle),
@@ -108,13 +119,15 @@
                 new Color[] { areaColor });
         }
 
-        public static void DrawRegularConvexPolygon(this CanvasItem canvas, Vector2 center, float radius, int verticesCount, float rotationAngle, Color lineColor, Color areaColor, float lineWidth = 1, bool antialiased = false)
+        public static void DrawRegularConvexPolygon(this CanvasItem canvas, Vector2 center, float radius, int verticesCount, float rotationAngle, Color lineColor, Color areaColor,
+            float lineWidth = 1, bool antialiased = false)
         {
             canvas.DrawRegularConvexPolygonArea(center, radius, verticesCount, rotationAngle, areaColor);
             canvas.DrawRegularConvexPolygonLine(center, radius, verticesCount, rotationAngle, lineColor, lineWidth, antialiased);
         }
 
-        public static void DrawCandlestick(this CanvasItem canvas, Vector2 low, float lowOffset, Vector2 high, float highOffset, float halfWidth, Color lineColor, Color bodyColor, float lineWidth = 1, bool antialiased = false)
+        public static void DrawCandlestick(this CanvasItem canvas, Vector2 low, float lowOffset, Vector2 high, float highOffset, float halfWidth, Color lineColor, Color bodyColor,
+            float lineWidth = 1, bool antialiased = false)
         {
             var vector = high - low;
             canvas.DrawRectangleArea(
