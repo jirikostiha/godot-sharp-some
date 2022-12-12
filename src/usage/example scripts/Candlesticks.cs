a035a5786c5a205163f8e6c83a130ff78e6b5f30
Jiri Kostiha <ijkdata@gmail.com>
2022-12-12 20:16:49 +0100

    refactor(examples): candlesticks animation

--- src/usage/example scripts/Candlesticks.cs
+++ src/usage/example scripts/Candlesticks.cs
@@ -4,23 +4,25 @@ using static Godot.Mathf;
 
 public class Candlesticks : ExampleNodeBase
 {
-    //private float _lowY, _size;
-    /private float _size;
+    private float _size;
     private float _open = 20;
-    private float _openDelta = 1f;
-    private float _close = 40;
-    private float _closeDelta = 1.2f;
+    private float _openDelta = 1.6f;
+    private float _close = 60;
+    private float _closeDelta = 2f;
 
     public Candlesticks()
     {
-        //_lowY = Bottom();
         _size = Top() - Bottom();
+        GD.Print("open:  " + _size);
     }
 
     protected override void NextState()
     {
-        _openDelta = NextDelta(_open, _openDelta);
-        _closeDelta = NextDelta(_close, _closeDelta);
+        _openDelta = UpdateDelta(_open, _openDelta);
+        _open += _openDelta;
+
+        _closeDelta = UpdateDelta(_close, _closeDelta);
+        _close += _closeDelta;
     }
 
     public override void _Draw()
@@ -30,30 +32,32 @@ public class Candlesticks : ExampleNodeBase
             LineColor);
 
         DrawMultiline(
-            Multiline.CrossedCandlestick(LeftTop(2), 20, LeftBottom(2), 10, 5, true),
-            LineColor);
-
-        DrawMultiline(
             Multiline.CrossedCandlestick(
-                MiddleTop(2), 
+                MiddleTop(2),
                 Min(_open, _close),
-                MiddleBottom(2), 
-                Max(_open, _close), 
-                5, 
+                MiddleBottom(2),
+                Min(_size - _open, _size - _close),
+                5,
                 _open < _close),
             LineColor);
 
         this.DrawCandlestick(
-            LeftTop(3), 
-            Min(_open, _close), LeftBottom(3), Max(_open, _close), 5, LineColor, 
+            MiddleTop(3),
+            Min(_open, _close),
+            MiddleBottom(3),
+            Min(_size - _open, _size - _close),
+            5, 
+            LineColor,
             _open < _close ? Color.ColorN("green") : Color.ColorN("red"));
     }
 
-    private float NextDelta(float current, float delta)
+    public void _on_Animate_pressed() => Animate = !Animate;
+
+    private float UpdateDelta(float value, float delta)
     {
-        if (current + delta > _size)
-             return -delta; //switch direction
-        if (current + delta < 0)
+        if (value > _size)
+            return -delta; //switch direction
+        if (value < 0)
             return -delta; //switch direction
 
         return delta;
