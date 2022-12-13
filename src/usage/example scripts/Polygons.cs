575978f4360b7b2cdab9eee7daa7ee43107c1eea
Jiri Kostiha <ijkdata@gmail.com>
2022-12-13 08:57:22 +0100

    refactor(examples): polygons animation

--- src/usage/example scripts/Polygons.cs
+++ src/usage/example scripts/Polygons.cs
@@ -1,13 +1,28 @@
-﻿using GodotSharpSome.Drawing2D;
+﻿using Godot;
+using GodotSharpSome.Drawing2D;
 
 public class Polygons : ExampleNodeBase
 {
+    private float _angle = 0;
+
+    protected override void NextState()
+    {
+        _angle += 0.1f;
+    }
+
     public override void _Draw()
     {
-        this.DrawRegularConvexPolygonLine(Middle(1), 30, 5, 0.2f, LineColor);
-
-        this.DrawRegularConvexPolygonArea(Middle(2), 30, 5, 0.2f, AreaColor);
-
-        this.DrawRegularConvexPolygon(Middle(3), 30, 5, 0.2f, LineColor, AreaColor);
+        for (int i = 4; i >= 0; i--)
+        {
+            var r = 5 + i * 10;
+            var n = i + 3;
+            var angle = i % 2 == 0 ? _angle : -_angle;
+            var areaColor = AreaColor.Darkened(i * 0.1f);
+            this.DrawRegularConvexPolygonLine(Middle(1), r, n, angle, LineColor);
+            this.DrawRegularConvexPolygonArea(Middle(2), r, n, angle, areaColor);
+            this.DrawRegularConvexPolygon(Middle(3), r, n, angle, LineColor, areaColor);
+        }
     }
+
+    public void _on_Animate_pressed() => Animate = !Animate;
 }
