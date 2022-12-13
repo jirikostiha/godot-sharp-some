581c8fe468eb98e93c404b42467912ca0cdbc128
jirikostiha <48102962+jirikostiha@users.noreply.github.com>
2022-12-14 00:36:06 +0100

    refactor(example): connections animations

--- src/usage/example scripts/Connections.cs
+++ src/usage/example scripts/Connections.cs
@@ -1,9 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using Godot;
 using GodotSharpSome.Drawing2D;
+using static Godot.Mathf;
 
 public class Connections : ExampleNodeBase
 {
+    private float _y = 0;
+    private float _r1, _r2, _r3, _r4, _r5;
+    private Vector2 _v1;
+
+    public Connections()
+    {
+        _v1 = Middle(3);
+        _r1 = 10;
+        _r2 = 12;
+        _r3 = 15;
+        _r4 = 8;
+        _r5 = 13;
+    }
+
+    protected override void NextState()
+    {
+        _y += 0.1f;
+    }
+
     public override void _Draw()
     {
         DrawSingleConnection(
@@ -14,9 +35,36 @@ public class Connections : ExampleNodeBase
             LeftBottom(2) + new Vector2(15, 16), 8f,
             RightBottom(2) + new Vector2(-15, 16), 8f,
             MiddleTop(2) + new Vector2(0, -16), 8f);
+
+        DrawChain(Middle(3));
     }
 
-    void DrawTriangleConnection(Vector2 a, float ar, Vector2 b, float br, Vector2 c, float cr)
+    public void _on_Animate_pressed() => Animate = !Animate;
+
+    private float _xOffset = 60;
+    private void DrawChain(Vector2 start)
+    {
+        var v2 = _v1 + new Vector2(_xOffset, Sin(_y) * 20);
+        var v3 = _v1 + new Vector2(2 * _xOffset, Sin(_y) * 28);
+        var v4 = _v1 + new Vector2(3 * _xOffset, Sin(_y) * 15);
+        var v5 = _v1 + new Vector2(4 * _xOffset, 0);
+
+        this.DrawCircle(_v1, _r1, LineColor, AreaColor);
+        this.DrawCircleLine(v2, _r2, LineColor);
+        this.DrawCircleLine(v3, _r3, LineColor);
+        this.DrawCircleLine(v4, _r4, LineColor);
+        this.DrawCircle(v5, _r5, LineColor, AreaColor);
+
+        //connections
+        var points = new List<Vector2>();
+        Multiline.AppendLine(points, _v1, _r1, v2, _r2);
+        Multiline.AppendLine(points, v2, _r2, v3, _r3);
+        Multiline.AppendLine(points, v3, _r3, v4, _r4);
+        Multiline.AppendLine(points, v4, _r4, v5, _r5);
+        DrawMultiline(points.ToArray(), LineColor);
+    }
+
+    private void DrawTriangleConnection(Vector2 a, float ar, Vector2 b, float br, Vector2 c, float cr)
     {
         this.DrawCircleLine(a, ar, LineColor);
         this.DrawCircleLine(b, br, LineColor);
@@ -33,7 +81,7 @@ public class Connections : ExampleNodeBase
         DrawMultiline(points.ToArray(), LineColor);
     }
 
-    void DrawSingleConnection(Vector2 a, float ar, Vector2 b, float br)
+    private void DrawSingleConnection(Vector2 a, float ar, Vector2 b, float br)
     {
         this.DrawCircleLine(a, ar, LineColor);
         this.DrawCircleLine(b, br, LineColor);
