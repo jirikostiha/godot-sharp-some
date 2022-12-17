45462b85d609301f22fba793991ff5aeb3cc601a
jirikostiha <48102962+jirikostiha@users.noreply.github.com>
2022-12-17 20:07:25 +0100

    refactor(examples): crosses animation

--- src/usage/example scripts/Crosses.cs
+++ src/usage/example scripts/Crosses.cs
@@ -2,27 +2,32 @@
 using System.Collections.Generic;
 using Godot;
 using GodotSharpSome.Drawing2D;
+using static Godot.Mathf;
 
 public class Crosses : ExampleNodeBase
 {
-    protected record Shot { public Vector2 Position; public Vector2 Destination; public Color Color; }
+    protected record Shot { 
+        public Vector2 Position; 
+        public Vector2 Destination; 
+        public int Type; }
 
     private List<Shot> _shots = new();
 
-    private Queue<(Vector2 Position, Color Color)> _markers = new();
+    private Queue<(Vector2 Position, int Type)> _markers = new();
 
     private Vector2 _cross1, _cross2;
     private Vector2 _origin1, _origin2;
     private Vector2 _target1, _target2;
     private Color _color1 = Color.ColorN("blue");
     private Color _color2 = Color.ColorN("red");
+    private int _counter;
 
     public Crosses()
     {
-        _origin1 = LeftTop(3);
-        _cross1 = _target1 = LeftMiddle(3);
-        _origin2 = MiddleTop(3);
-        _cross2 = _target2 = Middle(3);
+        _origin1 = MiddleTop(3);
+        _cross1 = _target1 = Middle(3);
+        _origin2 = MiddleTop(4);
+        _cross2 = _target2 = Middle(4);
     }
 
     protected override void NextState(float delta)
@@ -34,7 +39,7 @@ public class Crosses : ExampleNodeBase
             shot.Position = shot.Position.LinearInterpolate(shot.Destination, 0.05f);
             if (shot.Position.DistanceTo(shot.Destination) < 0.5f)
             {
-                _markers.Enqueue((shot.Position, shot.Color));
+                _markers.Enqueue((shot.Position, shot.Type));
                 if (_markers.Count > 4)
                     _markers.Dequeue();
                 
@@ -42,42 +47,91 @@ public class Crosses : ExampleNodeBase
             }
         }
 
+        // Aim and fire 1
         if (_cross1.DistanceTo(_target1) > 0.5f)
             //move cross
             _cross1 = _cross1.LinearInterpolate(_target1, 0.25f);
         else
         {
             //fire
-            var shot = new Shot() { Position = _origin1, Destination = _cross1, Color = _color1 };
+            var shot = new Shot() { Position = _origin1, Destination = _cross1, Type = 1 };
             _shots.Add(shot);
             //new target
             _target1 = NextVectorBetween(LeftBottom(3), RightMiddle(3));
         }
+
+        // Aim 2
+        if (_cross2.DistanceTo(_target2) > 0.5f)
+            _cross2 =  _cross2.LinearInterpolate(_target2, 0.1f);
+        else
+            //new target 2
+            _target2 = NextVectorBetween(LeftBottom(4), RightMiddle(4));
+
+        // Fire 2
+        if (_counter % 5 == 0)
+        {
+            var shot = new Shot() { Position = _origin2, Destination = _cross2, Type = 2 };
+            _shots.Add(shot);
+        }
+
+        _counter++;
     }
 
     public override void _Draw()
     {
+        // I
         DrawMultiline(
             Multiline.Cross(Middle(1), radius: 15),
             LineColor);
 
+        // II
         DrawMultiline(
             Multiline.Cross2(Middle(2), outerRadius: 15, innerRadius: 5),
             LineColor, width: 2);
 
-        //crosses
-        DrawMultiline(Multiline.Cross2(_cross1, 10, 3), _color1);
-        DrawMultiline(Multiline.Cross2(_cross2, 10, 2), _color2);
-
-        //shots
-        foreach (var shot in _shots)
-            //DrawMultiline(Multiline.Dot(shot.Position), shot.Color);
-            this.DrawCircleArea(shot.Position, 3, shot.Color);
-
-        //markers
-        foreach (var marker in _markers)
-            DrawMultiline(Multiline.Cross(marker.Position, 3), marker.Color);
+        // III
+        DrawShooting();
     }
 
     public void _on_Animate_pressed() => Animate = !Animate;
+
+    private void DrawShooting()
+    {
+        //aiming crosses
+        DrawMultiline(Multiline.Cross2(_cross1, 10, 3), _color1);
+        DrawMultiline(Multiline.Cross2(_cross2, 7, 2), _color2);
+
+        //guns
+        this.DrawRectangle(_origin1, 18, 8, _origin1.DirectionTo(_cross1).Angle(), LineColor, _color1);
+        this.DrawRectangle(_origin2, 10, 4, _origin2.DirectionTo(_cross2).Angle(), LineColor, _color2);
+
+        //shots
+        foreach (var shot in _shots)
+            switch (shot.Type)
+            {
+                case 1:
+                    this.DrawCircleArea(shot.Position, 3, _color1);
+                    break;
+                case 2:
+                    DrawMultiline(Multiline.Dot(shot.Position), _color2);
+                    break;
+
+
+                default: break;
+            }
+
+        //markers
+        foreach (var marker in _markers)
+        {
+            switch (marker.Type)
+            {
+                case 1:
+                    DrawMultiline(Multiline.Cross(marker.Position, 3), _color1);
+                    break;
+                case 2:
+                    DrawMultiline(Multiline.Dot(marker.Position), _color2);
+                    break;
+            }
+        }
+    }
 }
