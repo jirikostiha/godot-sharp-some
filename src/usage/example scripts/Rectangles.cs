b81d5e0b22caf41002aebb45ccc105ff230c9da9
Jiri Kostiha <ijkdata@gmail.com>
2022-12-18 01:27:07 +0100

    refactor(example): rectangles animations

--- src/usage/example scripts/Rectangles.cs
+++ src/usage/example scripts/Rectangles.cs
@@ -1,18 +1,69 @@
-﻿using GodotSharpSome.Drawing2D;
+﻿using System.Collections.Generic;
+using Godot;
+using GodotSharpSome.Drawing2D;
 using static Godot.Mathf;
 
 public class Rectangles : ExampleNodeBase
 {
+    private const float Tolerance = 1;
+
+    private float _length, _nextLength;
+    private float _width, _nextWidth;
+    private float _angle, _nextAngle;
+
+    private Queue<(float Length, float Width, float Angle)> _tracking = new();
+
+    public Rectangles()
+    {
+        _length = _nextLength = 68f;
+        _width = _nextWidth = 24f;
+        _angle = _nextAngle = Pi / 5.1f;
+    }
+
+    protected override void NextState(float delta)
+    {
+        if (_tracking.Count > 10)
+            _tracking.Dequeue();
+        
+        _tracking.Enqueue((_length, _width, _angle));
+
+        if (Abs(_nextLength - _length) < Tolerance)
+            _nextLength = NextFloat(4, 80);
+
+        if (Abs(_nextWidth - _width) < Tolerance)
+            _nextWidth = NextFloat(4, 40);
+
+        if (Abs(_nextAngle - _angle) < Tolerance)
+            _nextAngle = NextFloat(0, 2 * Pi);
+
+        _length = Lerp(_length, _nextLength, 0.1f);
+        _width = Lerp(_width, _nextWidth, 0.04f);
+        _angle = Lerp(_angle, _nextAngle, 0.1f);
+    }
+
     public override void _Draw()
     {
-        var length = 68f;
-        var width = 24f;
-        var rotationAngle = Pi / 5.1f;
+        this.DrawRectangleLine(Middle(1), _length, _width, _angle, LineColor);
 
-        this.DrawRectangleLine(Middle(1), length, width, rotationAngle, LineColor);
+        this.DrawRectangleArea(Middle(2), _length, _width, _angle, AreaColor);
 
-        this.DrawRectangleArea(Middle(2), length, width, rotationAngle, AreaColor);
+        this.DrawRectangle(Middle(3), _length, _width, _angle, LineColor, AreaColor);
 
-        this.DrawRectangle(Middle(3), length, width, rotationAngle, LineColor, AreaColor);
+        _DrawTracks();
     }
+
+    public void _DrawTracks()
+    {
+        var lineColor = new Color(LineColor, 0.1f);
+        var areaColor = new Color(AreaColor, 0.1f);
+        foreach (var item in _tracking)
+        {
+            lineColor = lineColor.Lightened(1 / (float)_tracking.Count);
+            this.DrawRectangleLine(Middle(1), item.Length, item.Width, item.Angle, lineColor);
+            this.DrawRectangleArea(Middle(2), item.Length, item.Width, item.Angle, areaColor);
+            this.DrawRectangle(Middle(3), item.Length, item.Width, item.Angle, lineColor, areaColor);
+        }
+    }
+
+    public void _on_Animate_pressed() => Animate = !Animate;
 }
