a45da2ee72e22c355f85c8fc231d9a599ee682b4
jirikostiha <48102962+jirikostiha@users.noreply.github.com>
2022-12-19 13:42:24 +0100

    refactor(examples): arrows animations

--- src/usage/example scripts/Arrows.cs
+++ src/usage/example scripts/Arrows.cs
@@ -5,32 +5,35 @@ using static Godot.Mathf;
 
 public class Arrows: ExampleNodeBase
 {
+    private float _time;
+    private float _headRadius = 15;
+    private float _arrowAngle = Pi * 0.1f;
+
+    protected override void NextState(float delta)
+    {
+        if (_time < 0.5)
+        {
+            _time += delta;
+            return;
+        }
+
+        _time = 0;
+        _headRadius = NextFloat(4, 25);
+        _arrowAngle = NextFloat(Pi * 0.04f, Pi * 0.25f);
+    }
+
     public override void _Draw()
     {
         DrawMultiline(
-            Multiline.Arrow(LeftBottom(1), RightTop(1), headRadius: 15),
+            Multiline.Arrow(LeftBottom(1), RightTop(1), _headRadius, _arrowAngle),
             LineColor);
 
         DrawMultiline(
-            Multiline.Arrow(LeftBottom(2), RightTop(2), headRadius: 10, arrowAngle: Pi * 5 / 6),
+            Multiline.Arrow(LeftBottom(2), Middle(2), _headRadius, Pi - _arrowAngle),
             LineColor);
 
         DrawMultiline(
-            Multiline.DoubleArrow(LeftBottom(3), RightTop(3), headRadius: 15),
+            Multiline.DoubleArrow(LeftBottom(3), RightTop(3), _headRadius, _arrowAngle),
             LineColor);
-
-        DrawDimmensionLength(4);
-    }
-
-    void DrawDimmensionLength(int column)
-    {
-        var a = LeftMiddle(column);
-        var b = RightMiddle(column);
-        var points = new List<Vector2>(14);
-        Multiline.AppendLine(points, a + new Vector2(0, 8), a + new Vector2(0, -4));
-        Multiline.AppendLine(points, b + new Vector2(0, 8), b + new Vector2(0, -4));
-        Multiline.AppendDoubleArrow(points, a, b, 16);
-        DrawMultiline(points.ToArray(), LineColor);
-        DrawString(GetFont(null), Middle(column) + new Vector2(-8, -3), "42", TextColor);
     }
 }
