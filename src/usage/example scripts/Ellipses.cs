1d4cdbfa54529fbbe71665d196a155a5ae24ebac
jirikostiha <48102962+jirikostiha@users.noreply.github.com>
2023-01-06 14:04:49 +0100

    example(ellipse): ellipse animation

---
+++ src/usage/example scripts/Ellipses.cs
@@ -0,0 +1,26 @@
+﻿using Godot;
+using GodotSharpSome.Drawing2D;
+using static Godot.Mathf;
+
+public class Ellipses : ExampleNodeBase
+{
+    private float _time;
+    private float _radius = 40;
+
+    protected override void NextState(float delta)
+    {
+        _time += delta;
+    }
+
+    public override void _Draw()
+    {
+        // I
+        this.DrawEllipseLine(Middle(1), _radius, _radius * Sin(_time + 0.5f), _time * 3.3f, LineColor);
+
+        // II
+        this.DrawEllipseArea(Middle(2), _radius, _radius * Sin(_time + 0.5f), _time * 3.3f, AreaColor);
+
+        // III
+        this.DrawEllipse(Middle(3), _radius, _radius * Sin(_time + 0.5f), _time * 3.3f, LineColor, AreaColor);
+    }
+}
