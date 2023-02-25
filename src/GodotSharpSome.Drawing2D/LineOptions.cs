6e9c3882d0c9026914c0f0a7ea97e1049bb3bf08
Jiri Kostiha <ijkdata@gmail.com>
2023-02-25 20:29:00 +0100

    refactor: line options

---
+++ src/GodotSharpSome.Drawing2D/LineOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace GodotSharpSome.Drawing2D
+{
+    using Godot;
+
+    public record LineOptions
+    {
+        public Color? Color { get; set; }
+
+        public LineType Type { get; set; } = LineType.Solid;
+
+        public float Width { get; set; } = 1;
+    }
+}
