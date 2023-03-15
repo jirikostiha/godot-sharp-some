ef06d72362265cf07c19bfa9369946555357dcfd
Jiri Kostiha <ijkdata@gmail.com>
2023-03-15 15:57:07 +0100

    test: test project with initial test, add to wfs

---
+++ src/quality/GodotSharpSome.Drawing2D.Tests/MultilineTest.cs
@@ -0,0 +1,13 @@
+using Godot;
+
+namespace GodotSharpSome.Drawing2D.Tests
+{
+    public class MultilineTest
+    {
+        [Fact]
+        public void AppendLine_TwoPoints()
+        {
+            Assert.Equal(2, new Multiline().AppendLine(Vector2.Zero, Vector2.One).Points.Length);
+        }
+    }
+}
