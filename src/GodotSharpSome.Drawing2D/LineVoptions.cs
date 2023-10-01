e11908e7b010f96d07122245d06a7a8a882e6a44
Jiri Kostiha <ijkdata@gmail.com>
2023-10-01 16:44:39 +0200

    refactor: LineVOptions - preset values, rename Type to line LineType

--- src/GodotSharpSome.Drawing2D/LineVoptions.cs
+++ src/GodotSharpSome.Drawing2D/LineVoptions.cs
@@ -5,9 +5,19 @@
 /// </summary>
 public record LineVOptions
 {
-    public Color Color { get; set; } = Colors.Black;
+    /// <summary>
+    /// Preset options to black solid line of width 1.
+    /// </summary>
+    public static LineVOptions BlackSolid1 => new() { LineType = LineType.Solid, Width = 1, Color = Colors.Black };
 
-    public LineType Type { get; set; } = LineType.Solid;
+    /// <summary>
+    /// Preset options to white solid line of width 1.
+    /// </summary>
+    public static LineVOptions WhiteSolid1 => new() { LineType = LineType.Solid, Width = 1, Color = Colors.White };
+
+    public Color Color { get; set; } = Colors.Gray;
+
+    public LineType LineType { get; set; } = LineType.Solid;
 
     public float Width { get; set; } = 1;
 }
