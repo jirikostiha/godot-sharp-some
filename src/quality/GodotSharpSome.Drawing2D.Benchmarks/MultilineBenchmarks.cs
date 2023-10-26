e4976c4a68c8c073fddc9c3187087b57104a18a4
Jiri Kostiha <ijkdata@gmail.com>
2023-10-26 20:49:31 +0200

    perf(bench): multiline benchmarks, wf

---
+++ src/quality/GodotSharpSome.Drawing2D.Benchmarks/MultilineBenchmarks.cs
@@ -0,0 +1,99 @@
+﻿using BenchmarkDotNet.Attributes;
+using Godot;
+
+namespace GodotSharpSome.Drawing2D.Benchmarks;
+
+[JsonExporterAttribute.Full]
+[JsonExporterAttribute.FullCompressed]
+//[JsonExporter("-custom", indentJson: true, excludeMeasurements: true)]
+[MemoryDiagnoser]
+public class MultilineBenchmarks
+{
+    private readonly List<Vector2> _lines = new(17);
+
+    [Params(1000, 10_000, 100_000)]
+    public int N { get; set; }
+
+    [Benchmark]
+    public Multiline? CreateDefault()
+    {
+        Multiline? ml = null;
+        for (int i = 0; i < N; i++)
+        {
+            ml = new Multiline();
+        }
+        return ml;
+    }
+
+    [Benchmark]
+    public Multiline? CreateDefaultWithList()
+    {
+        Multiline? ml = null;
+        for (int i = 0; i < N; i++)
+        {
+            ml = new Multiline(_lines);
+        }
+        return ml;
+    }
+
+    [Benchmark]
+    public Multiline? CreateDefaultFourLineTypes()
+    {
+        Multiline? ml = null;
+        for (int i = 0; i < N; i++)
+        {
+            ml = Multiline.FourLineTypes();
+        }
+        return ml;
+    }
+
+    [Benchmark]
+    public Multiline? CreateCustomFourLineTypes()
+    {
+        Multiline? ml = null;
+        for (int i = 0; i < N; i++)
+        {
+            ml = Multiline.FourLineTypes(6, 8);
+        }
+        return ml;
+    }
+
+    [Benchmark]
+    public Vector2[] AppendSolidLines()
+    {
+        var ml = new Multiline();
+        for (int i = 0; i < N; i++)
+        {
+            ml.AppendLine(Vector2.Zero, Vector2.One);
+            ml.AppendTriangle(Vector2.Zero, Vector2.One, Vector2.Up);
+        }
+
+        return ml.Points();
+    }
+
+    [Benchmark]
+    public Vector2[] AppendDashedLines()
+    {
+        var ml = new Multiline(new DashedLine());
+        for (int i = 0; i < N; i++)
+        {
+            ml.AppendLine(Vector2.Zero, Vector2.One);
+            ml.AppendTriangle(Vector2.Zero, Vector2.One, Vector2.Up);
+        }
+
+        return ml.Points();
+    }
+
+    [Benchmark]
+    public Vector2[] AppendDashedLinesDefaultAppender()
+    {
+        var ml = new Multiline(DashedLine.Default);
+        for (int i = 0; i < N; i++)
+        {
+            ml.AppendLine(Vector2.Zero, Vector2.One);
+            ml.AppendTriangle(Vector2.Zero, Vector2.One, Vector2.Up);
+        }
+
+        return ml.Points();
+    }
+}
---
+++ src/quality/GodotSharpSome.Drawing2D.Benchmarks/Program.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using System.Runtime.CompilerServices;
+
+namespace GodotSharpSome.Drawing2D.Benchmarks;
+
+public class Program
+{
+    private static string GetSourceFilePathName([CallerFilePath] string? callerFilePath = null) => callerFilePath ?? string.Empty;
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("directory: " + Directory.GetCurrentDirectory());
+
+        string outputFolder = @"./../../../benchmarks";
+        if (!Directory.GetCurrentDirectory().EndsWith(typeof(Program).Assembly.GetName().Name ?? string.Empty))
+        {
+            // workaround for executing from VS because the output path of binaries is in different folder than usual
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(GetSourceFilePathName()) ?? string.Empty);
+            Console.WriteLine("changed to: " + Directory.GetCurrentDirectory());
+        }
+
+        ManualConfig config = ManualConfig.Create(DefaultConfig.Instance.WithArtifactsPath(outputFolder))
+            .WithOption(ConfigOptions.JoinSummary, true)
+            .WithOption(ConfigOptions.DisableLogFile, true);
+
+        BenchmarkRunner.Run(typeof(Program).Assembly, config);
+    }
+}
