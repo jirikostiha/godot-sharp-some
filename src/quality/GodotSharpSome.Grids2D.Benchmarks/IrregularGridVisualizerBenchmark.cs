f49d6d26072a3806efdc274ff5496f4aa4af34e1
Jiri Kostiha <ijkdata@gmail.com>
2023-11-14 20:22:20 +0100

    bench

---
+++ src/quality/GodotSharpSome.Grids2D.Benchmarks/IrregularGridVisualizerBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using Godot;
+
+namespace GodotSharpSome.Grids2D.Benchmarks;
+
+[MemoryDiagnoser]
+public class IrregularGridVisualizerBenchmark
+{
+    private readonly Control _control = new();
+    private readonly OrthogonalGridVOptions _voptions = new();
+    private readonly IrregularGridOptions _options = new();
+    private IrregularGridVisualizer _vizer;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _vizer = new(_control, _voptions);
+    }
+
+    [Benchmark]
+    public IrregularGridVisualizer CreateDefault()
+    {
+        return new IrregularGridVisualizer(_control, _voptions);
+    }
+
+    [Benchmark]
+    public void Draw()
+    {
+        new IrregularGridVisualizer(_control, _voptions).Draw(_options);
+    }
+}
---
+++ src/quality/GodotSharpSome.Grids2D.Benchmarks/OrthogonalGridVisualizerBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using Godot;
+
+namespace GodotSharpSome.Grids2D.Benchmarks;
+
+[MemoryDiagnoser]
+public class OrthogonalGridVisualizerBenchmark
+{
+    private readonly Control _control = new();
+    private readonly OrthogonalGridVOptions _voptions = new();
+    private readonly OrthogonalGridOptions _options = new();
+    private OrthogonalGridVisualizer _vizer;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _vizer = new(_control, _voptions);
+    }
+
+    [Benchmark]
+    public OrthogonalGridVisualizer CreateDefault()
+    {
+        return new OrthogonalGridVisualizer(_control, _voptions);
+    }
+
+    [Benchmark]
+    public void CreateDefaul()
+    {
+        _vizer.Draw(_options);
+    }
+}
