d68c4ae0f4c6349fb97dc184449628c778a70aa2
github-action-benchmark <github@users.noreply.github.com>
2026-08-24 16:27:39 +0000

    add Benchmark.Net (benchmarkdotnet) benchmark result for 43280535b778ddd7695d02a1f9c15b32b5b66ea0

---
+++ dev/bench/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1787588859759,
+  "repoUrl": "https://github.com/jirikostiha/godot-sharp-some",
+  "entries": {
+    "Benchmark.Net": [
+      {
+        "commit": {
+          "author": {
+            "email": "ijkdata@gmail.com",
+            "name": "Jiri Kostiha",
+            "username": "jirikostiha"
+          },
+          "committer": {
+            "email": "ijkdata@gmail.com",
+            "name": "Jiri Kostiha",
+            "username": "jirikostiha"
+          },
+          "distinct": true,
+          "id": "43280535b778ddd7695d02a1f9c15b32b5b66ea0",
+          "message": "docs: add documentation website badge, section, and PackageProjectUrl",
+          "timestamp": "2026-08-24T18:11:25+02:00",
+          "tree_id": "7f56052f04293ce2f432cfe63a482c801c03c59a",
+          "url": "https://github.com/jirikostiha/godot-sharp-some/commit/43280535b778ddd7695d02a1f9c15b32b5b66ea0"
+        },
+        "date": 1787588859004,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "GodotSharpSome.Drawing2D.Benchmarks.MultilineBenchmarks.CreateDefault",
+            "value": 16.197271571556726,
+            "unit": "ns",
+            "range": "± 0.3262126216138353"
+          },
+          {
+            "name": "GodotSharpSome.Drawing2D.Benchmarks.MultilineBenchmarks.CreateDefaultWithList",
+            "value": 10.9756661405166,
+            "unit": "ns",
+            "range": "± 0.3008867144359934"
+          },
+          {
+            "name": "GodotSharpSome.Drawing2D.Benchmarks.MultilineBenchmarks.CreateDefaultWithListAndPalette",
+            "value": 16.05640183289846,
+            "unit": "ns",
+            "range": "± 0.30272710260486185"
+          },
+          {
+            "name": "GodotSharpSome.Drawing2D.Benchmarks.MultilineBenchmarks.CreateDefaultFourLineTypes",
+            "value": 41.43569712638855,
+            "unit": "ns",
+            "range": "± 0.8046619902471605"
+          },
+          {
+            "name": "GodotSharpSome.Drawing2D.Benchmarks.MultilineBenchmarks.CreateCustomFourLineTypes",
+            "value": 57.04777689861215,
+            "unit": "ns",
+            "range": "± 1.5298193603769281"
+          }
+        ]
+      }
+    ]
+  }
+}
